a63d08477bf452b495cc4252460d316fa036f356
krxxg04 <u202312629@upc.edu.pe>
2026-05-25 22:40:28 -0500

    style: format code for consistency in configuration files.

--- alert/infrastructure/persistence/gorm/configuration/db.go
+++ alert/infrastructure/persistence/gorm/configuration/db.go
@@ -1,10 +1,10 @@
 package configuration
 
 import (
-    "gorm.io/driver/postgres"
-    "gorm.io/gorm"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
 )
 
 func NewDatabase(databaseURL string) (*gorm.DB, error) {
-    return gorm.Open(postgres.Open(databaseURL), &gorm.Config{})
+	return gorm.Open(postgres.Open(databaseURL), &gorm.Config{})
 }
