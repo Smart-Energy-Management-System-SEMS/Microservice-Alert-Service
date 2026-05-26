81c8c1296a931a15b4643249038a54e9cf1792cf
krxxg04 <u202312629@upc.edu.pe>
2026-05-25 22:39:34 -0500

    style: format code for consistency in outbound services interfaces.

--- alert/application/outboundservices/notifications.go
+++ alert/application/outboundservices/notifications.go
@@ -3,9 +3,9 @@ package outboundservices
 import "context"
 
 type EmailSender interface {
-    Send(ctx context.Context, to string, subject string, body string) error
+	Send(ctx context.Context, to string, subject string, body string) error
 }
 
 type SmsSender interface {
-    Send(ctx context.Context, to string, body string) error
+	Send(ctx context.Context, to string, body string) error
 }
