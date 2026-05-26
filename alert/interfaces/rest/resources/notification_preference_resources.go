007a7a2620128ef5e0ff12cb45f17defb8207b9d
krxxg04 <u202312629@upc.edu.pe>
2026-05-25 22:45:01 -0500

    feat: standardize struct formatting across alert resource files for improved readability.

--- alert/interfaces/rest/resources/notification_preference_resources.go
+++ alert/interfaces/rest/resources/notification_preference_resources.go
@@ -3,20 +3,20 @@ package resources
 import "time"
 
 type CreateNotificationPreferenceRequest struct {
-    UserID          string `json:"user_id" binding:"required"`
-    Channel         string `json:"channel" binding:"required"`
-    Enabled         bool   `json:"enabled"`
-    MinSeverity     string `json:"min_severity"`
-    QuietHoursStart string `json:"quiet_hours_start"`
-    QuietHoursEnd   string `json:"quiet_hours_end"`
+	UserID          string `json:"user_id" binding:"required"`
+	Channel         string `json:"channel" binding:"required"`
+	Enabled         bool   `json:"enabled"`
+	MinSeverity     string `json:"min_severity"`
+	QuietHoursStart string `json:"quiet_hours_start"`
+	QuietHoursEnd   string `json:"quiet_hours_end"`
 }
 
 type NotificationPreferenceResponse struct {
-    PreferenceID    string     `json:"preference_id"`
-    UserID          string     `json:"user_id"`
-    Channel         string     `json:"channel"`
-    Enabled         bool       `json:"enabled"`
-    MinSeverity     string     `json:"min_severity"`
-    QuietHoursStart *time.Time `json:"quiet_hours_start"`
-    QuietHoursEnd   *time.Time `json:"quiet_hours_end"`
+	PreferenceID    string     `json:"preference_id"`
+	UserID          string     `json:"user_id"`
+	Channel         string     `json:"channel"`
+	Enabled         bool       `json:"enabled"`
+	MinSeverity     string     `json:"min_severity"`
+	QuietHoursStart *time.Time `json:"quiet_hours_start"`
+	QuietHoursEnd   *time.Time `json:"quiet_hours_end"`
 }
--- alert/interfaces/rest/resources/response.go
+++ alert/interfaces/rest/resources/response.go
@@ -1,5 +1,5 @@
 package resources
 
 type ErrorResponse struct {
-    Error string `json:"error"`
+	Error string `json:"error"`
 }
